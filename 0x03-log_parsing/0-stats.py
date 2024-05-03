ad15d083d1bfaa190b66847c95529b24edac2464
milkawinoh <milkawinoh@gmail.com>
2024-05-03 04:55:48 +0300

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,22 +8,22 @@ from typing import Optional, Tuple, Dict
 total_file_size: int = 0
 status_code_counts: Dict[int, int] = {}
 
+
 def parse_log_entry(line: str) -> Optional[Tuple[str, int, int]]:
     """
-    Parse a log entry string and extract IP address, status 
-    code, and file size.
+    Parse a log entry string and extract IP address, status code,
+    and file size.
 
     Args:
         line (str): Log entry string to parse.
 
     Returns:
-        Optional[Tuple[str, int, int]]: A tuple containing IP 
-        address, status code, and file size,
-            or None if the log entry does not match the expected 
-            format.
+        Optional[Tuple[str, int, int]]: A tuple containing IP address,
+        status code, and file size,
+            or None if the log entry does not match the expected format.
     """
-    pattern = r'^([\d\.]+) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)'
-    match = re.match(pattern, line)
+    patn = r'^([\d\.]+) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)'
+    match = re.match(patn, line)
     if match:
         ip_address = match.group(1)
         status_code = int(match.group(3))
@@ -31,19 +31,19 @@ def parse_log_entry(line: str) -> Optional[Tuple[str, int, int]]:
         return ip_address, status_code, file_size
     return None
 
+
 def print_statistics() -> None:
     """
-    Print computed statistics: total file size 
-    and count of each status code.
+    Print computed statistics: total file size and count of each status code.
     """
     print(f"Total file size: {total_file_size}")
     for code in sorted(status_code_counts):
         print(f"{code}: {status_code_counts[code]}")
 
+
 def signal_handler(sig: int, frame) -> None:
     """
-    Signal handler to print statistics and 
-    exit upon receiving SIGINT.
+    Signal handler to print statistics and exit upon receiving SIGINT.
 
     Args:
         sig (int): Signal number.
@@ -52,10 +52,11 @@ def signal_handler(sig: int, frame) -> None:
     print_statistics()
     sys.exit(0)
 
+
 def main() -> None:
     """
-    Main function to read log entries from stdin, 
-    parse them, and compute statistics.
+    Main function to read log entries from stdin, parse them,
+    and compute statistics.
     """
     signal.signal(signal.SIGINT, signal_handler)
     line_count: int = 0
@@ -81,6 +82,7 @@ def main() -> None:
         # Handle KeyboardInterrupt (Ctrl+C) gracefully
         print_statistics()
         sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
